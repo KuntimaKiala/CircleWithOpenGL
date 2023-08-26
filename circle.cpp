67ad00246bc87b481e66105edf9feaace3ef0db1
KuntimaKiala <Kuntima.kiala@gmail.com>
2023-08-26 11:44:42 +0200

    Seting Up the Frame Buffer for window resizing

--- circle.cpp
+++ circle.cpp
@@ -8,13 +8,49 @@ using std::endl ;
 using std::cout ;
 
 
-
-
+void init() ;
+void framebuffersize(GLFWwindow* window, int width, int height ) ;
 int main(){
 
+  /*initializing glfw and creating the window*/
+  init();
+  GLFWwindow* window = glfwCreateWindow(constant::width, constant::height, "circle", NULL, NULL) ;
+  glfwMakeContextCurrent(window) ;
+  glfwSetFramebufferSizeCallback(window, framebuffersize) ;
+
+
+
+  while(!glfwWindowShouldClose(window)){
+
+    glfwPollEvents();
+    glfwSwapBuffers(window) ;
+
+  }
+
+
+glfwTerminate();
+return 0 ;
+
+}
+
+
+void init(){
 
   cout << "Nous sommes Ubuntu" << endl;
+  
+  glfwInit() ;
+  glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwInitHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE) ;
+  
 
+
+}
+
+
+void framebuffersize(GLFWwindow* window, int width, int height){
+
+  glViewport(0,0, width, height) ;
 
 
 }
